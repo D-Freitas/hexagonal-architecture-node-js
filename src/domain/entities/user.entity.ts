770a67eeee809db64f0399f894501221ef4a990c
D-Freitas <davi.israel01@gmail.com>
2023-05-25 13:11:27 -0300

    feat(user_entity): ensure this returns an object with user attributes

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  constructor (
+    private readonly firstName: string,
+    private readonly lastName: string,
+    private readonly username: string,
+    private readonly age: number
+  ) {}
+}
